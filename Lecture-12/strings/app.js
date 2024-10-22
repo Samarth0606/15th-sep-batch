1d442f30af288f8f1334417b25e196b043c8ae1b
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-10-22 22:30:00 +0530

    lecture 12 js codes

---
+++ Lecture-12/strings/app.js
@@ -0,0 +1,73 @@
+// strings (char ❌)
+
+// let str = "samarth";
+// let str2 = 'samarth';
+// let str3 = `samarth`; //backticks
+
+// let str4 = 'x'; //typeof => string
+
+
+// let typeee = typeof(str4);
+// console.log(typeee);
+// //  or
+// console.log( typeof(str4) );
+
+
+
+// console.log(str);
+// console.log(str2);
+// console.log(str3);
+
+// -----------------------------------
+
+// let naam = "samarth vohra";
+// let age = 100;
+// let city = "New Delhi";
+
+// hi i am samarth vohra i live in new delhi and my age is 100
+
+//way-1
+// concatenation (concat  = + ) -> joins the strings
+// let output = "hi i am " + naam + " i live in " + city + " and my age is  " + age;
+// console.log(output);
+
+
+// -----------------------------------
+
+// string template literals
+
+// let name = "samarth vohra";
+// let age = 100;
+// let city = "New Delhi";
+
+// hi i am samarth vohra i live in new delhi and my age is 100
+// backticks
+// let output = `hi i am ${name} i live in ${city} and my age is ${age}`
+
+// console.log(output);
+
+
+// ---------------------------------
+let str = "samaRTh  vOHra";
+// length
+// console.log(str.length);
+
+// trim() => remove extra spaces from start and end
+// let newStr = str.trim();
+
+// console.log(newStr.length);
+
+
+
+// let newstr1 = str.toUpperCase()
+// let newstr2 = str.toLowerCase()
+// console.log(str);
+// console.log(newstr1);
+// console.log(newstr2);
+
+
+// record videos too (MDN)
+
+
+
+
